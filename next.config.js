916853c792e3553a423279847b86c7f1e8a296f9
nghiatt <nghiatt@bsscommerce.com>
2022-03-16 17:15:46 +0000

    change DB URL

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   reactStrictMode: true,
   env: {
-    mongodburl: "mongodb+srv://admin:Ntt141187@cluster0.eboyt.mongodb.net/loyalty-solana",
+    mongodburl: process.env.MONGO_DB_URL,
     appDomain: "app.sol-loyalty.com",
     apiVersion: "2021-10"
   }
